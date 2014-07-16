7f1166f8846593d21e3cd2912ad35a6ce3f1d6a4
Yedidya Weiner <ydweiner17@gmail.com>
2014-07-15 20:49:53 -0500

    made city and state into string

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 
   def weather
     results = Geocoder.search(request.remote_ip)
-    @results[city], @region_name[state]
+    @results["city"], @region_name["state"]
     weather_info = OpenWeather::Current.city("#{@results[city],}, #{@region_name[state]}")
     @temp_celsius = (weather_info["main"]["temp"].to_f - 273).round(2)
     @temp_farenheit = 1.8 * @temp_celsius + 32
