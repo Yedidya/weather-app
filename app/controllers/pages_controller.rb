ccc657264188a7f2c8de3697fe6ecf065ffecec7
Yedidya Weiner <ydweiner17@gmail.com>
2014-07-15 20:30:26 -0500

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+require 'open_weather'
+
+class PagesController < ApplicationController
+
+  def weather
+    weather_info = OpenWeather::Current.city("Chicago, IL")
+    @temp_celsius = (weather_info["main"]["temp"].to_f - 273).round(2)
+    @temp_farenheit = 1.8 * @temp_celsius + 32
+  end
+
+end
