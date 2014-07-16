f59ee89342f472a3be37eadc0306fccec657e04f
Yedidya Weiner <ydweiner17@gmail.com>
2014-07-15 20:46:43 -0500

    made the results specific to location

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,11 @@ require 'open_weather'
 class PagesController < ApplicationController
 
   def weather
-    weather_info = OpenWeather::Current.city("Chicago, IL")
+    results = Geocoder.search(request.remote_ip)
+    @results[city], @region_name[state]
+    weather_info = OpenWeather::Current.city("#{@results[city],}, #{@region_name[state]}")
     @temp_celsius = (weather_info["main"]["temp"].to_f - 273).round(2)
     @temp_farenheit = 1.8 * @temp_celsius + 32
   end
 
-end
+end 
